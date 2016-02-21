5f1cb481eb52851d53257773ba2c30bb12dcfd54
Vincent ARROCENA <varrocen@gmail.com>
2016-02-21 22:32:52 +0100

    Init project

---
+++ src/main/java/com/mowitnow/MowerApplication.java
@@ -0,0 +1,12 @@
+package com.mowitnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MowerApplication.class, args);
+	}
+}
