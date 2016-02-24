4fdc12b7da1729608e009d13bdfe502daaf48e5c
Vincent ARROCENA <varrocen@gmail.com>
2016-02-24 23:59:00 +0100

    Update Movable processor

--- src/main/java/com/mowitnow/MovableProcessor.java
+++ src/main/java/com/mowitnow/MovableProcessor.java
@@ -3,6 +3,7 @@ package com.mowitnow;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 import com.mowitnow.model.Field;
 import com.mowitnow.model.Movable;
 import com.mowitnow.parser.GrassParser;
+import com.mowitnow.parser.MowerParser;
 import com.mowitnow.reader.CommandFileReader;
 
 @Component
@@ -24,6 +26,9 @@ public class MovableProcessor {
 	@Autowired
 	private GrassParser grassParser;
 
+	@Autowired
+	private MowerParser mowerParser;
+
 	/**
 	 * Reads commands file and move mowers
 	 * 
@@ -35,9 +40,18 @@ public class MovableProcessor {
 			List<String> commands = commandFileReader.read(file);
 
 			Optional<Field> field = grassParser.parse(commands);
-			
+
 			if (field.isPresent()) {
-				
+				List<Movable> movables = mowerParser.parse(commands);
+
+				if (CollectionUtils.isNotEmpty(movables)) {
+
+					for (Movable movable : movables) {
+						moveMovable(field.get(), movable);
+					}
+				} else {
+					log.error("No mower no beautiful lawn");
+				}
 			} else {
 				log.error("Grass command is invalid : {}", commands.get(0));
 			}
@@ -47,39 +61,34 @@ public class MovableProcessor {
 		}
 	}
 
+	/**
+	 * Reads the instructions and moves the movable
+	 * 
+	 * @param field
+	 * @param movable
+	 */
 	public void moveMovable(Field field, Movable movable) {
-		log.info(field.toString());
-		log.debug("Start");
-		log.debug(movable.toString());
 
-		char[] instructions = movable.getInstructions().toUpperCase().toCharArray();
-		for (int i = 0; i < instructions.length; i++) {
-			switch (instructions[i]) {
-			case 'D':
-				movable.rotate90();
-				break;
-			case 'G':
-				movable.rotate270();
-				break;
-			case 'A':
-				if (field.isValidNextPosition(movable.getCoordinate())) {
-					movable.move();
-				} else {
-					log.debug("No move");
+		if (field != null && movable != null) {
+			char[] instructions = movable.getInstructions().toCharArray();
+			for (int i = 0; i < instructions.length; i++) {
+				switch (instructions[i]) {
+				case 'D':
+					movable.rotate90();
+					break;
+				case 'G':
+					movable.rotate270();
+					break;
+				case 'A':
+					if (field.isValidNextPosition(movable.getCoordinate())) {
+						movable.move();
+					}
+					break;
+				default:
+					break;
 				}
-				break;
-			default:
-				break;
 			}
-		}
-
-		log.info(movable.toString());
-		log.debug("Stop");
-	}
-
-	public void moveMovables(Field field, List<Movable> movables) {
-		for (Movable movable : movables) {
-			moveMovable(field, movable);
+			log.info(movable.toString());
 		}
 	}
 
--- src/test/java/com/mowitnow/MovableProcessorTest.java
+++ src/test/java/com/mowitnow/MovableProcessorTest.java
@@ -3,16 +3,13 @@ package com.mowitnow;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.mowitnow.model.Field;
 import com.mowitnow.model.Movable;
 import com.mowitnow.model.impl.Grass;
 import com.mowitnow.model.impl.Mower;
@@ -23,40 +20,50 @@ import com.mowitnow.utils.impl.CoordinateImpl;
 @SpringApplicationConfiguration(classes = MowerApplication.class)
 public class MovableProcessorTest {
 
+	private Grass grass;
+
+	@Before
+	public void setUp() {
+		grass = new Grass(5, 5);
+	}
+	
 	@Autowired
 	private MovableProcessor movableProcessor;
 
 	@Test
+	public void testMoveGrassNull() {
+		Movable mower = new Mower(new CoordinateImpl(3, 3, Orientation.E), "AADAADADDA");
+		movableProcessor.moveMovable(null, mower);
+		assertThat(mower.getCoordinate().getX(), equalTo(3));
+		assertThat(mower.getCoordinate().getY(), equalTo(3));
+		assertThat(mower.getCoordinate().getOrientation(), equalTo(Orientation.E));
+	}
+	
+	@Test
+	public void testMoveMowerNull() {
+		Movable mower = new Mower(new CoordinateImpl(3, 3, Orientation.E), "AADAADADDA");
+		movableProcessor.moveMovable(grass, null);
+		assertThat(mower.getCoordinate().getX(), equalTo(3));
+		assertThat(mower.getCoordinate().getY(), equalTo(3));
+		assertThat(mower.getCoordinate().getOrientation(), equalTo(Orientation.E));
+	}
+	
+	@Test
 	public void testMoveMower() {
-		Field grass = new Grass(5, 5);
-		Movable mower = new Mower(new CoordinateImpl(1, 2, Orientation.NORTH), "GAGAGAGAA");
-
+		Movable mower = new Mower(new CoordinateImpl(1, 2, Orientation.N), "GAGAGAGAA");
 		movableProcessor.moveMovable(grass, mower);
-
 		assertThat(mower.getCoordinate().getX(), equalTo(1));
 		assertThat(mower.getCoordinate().getY(), equalTo(3));
-		assertThat(mower.getCoordinate().getOrientation(), equalTo(Orientation.NORTH));
+		assertThat(mower.getCoordinate().getOrientation(), equalTo(Orientation.N));
 	}
 
 	@Test
-	public void testMoveMowers() {
-		Grass grass = new Grass(5, 5);
-		Movable mower1 = new Mower(new CoordinateImpl(1, 2, Orientation.NORTH), "GAGAGAGAA");
-		Movable mower2 = new Mower(new CoordinateImpl(3, 3, Orientation.EAST), "AADAADADDA");
-
-		List<Movable> mowers = new ArrayList<>();
-		mowers.add(mower1);
-		mowers.add(mower2);
-
-		movableProcessor.moveMovables(grass, mowers);
-
-		assertThat(mower1.getCoordinate().getX(), equalTo(1));
-		assertThat(mower1.getCoordinate().getY(), equalTo(3));
-		assertThat(mower1.getCoordinate().getOrientation(), equalTo(Orientation.NORTH));
-
-		assertThat(mower2.getCoordinate().getX(), equalTo(5));
-		assertThat(mower2.getCoordinate().getY(), equalTo(1));
-		assertThat(mower2.getCoordinate().getOrientation(), equalTo(Orientation.EAST));
+	public void testMoveMowerWithInvalidNextPosition() {
+		Movable mower = new Mower(new CoordinateImpl(3, 3, Orientation.E), "AADAADADDA");
+		movableProcessor.moveMovable(grass, mower);
+		assertThat(mower.getCoordinate().getX(), equalTo(5));
+		assertThat(mower.getCoordinate().getY(), equalTo(1));
+		assertThat(mower.getCoordinate().getOrientation(), equalTo(Orientation.E));
 	}
 
 }
