6a2df5a749786262486e0c3b879672ba91d6bb8f
Vincent ARROCENA <varrocen@gmail.com>
2016-02-24 09:00:17 +0100

    Update Command runner

--- src/main/java/com/mowitnow/MowerRunner.java
+++ src/main/java/com/mowitnow/MowerRunner.java
@@ -1,24 +1,39 @@
 package com.mowitnow;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
-import com.mowitnow.reader.CommandFileReader;
 
 @Component
 public class MowerRunner implements CommandLineRunner {
 
-	@Value("${command.file}")
-	private String commandFile;
+	private static final Logger log = LoggerFactory.getLogger(MowerRunner.class);
 
 	@Autowired
-	private CommandFileReader commandFileReader;
+	private ApplicationArguments applicationArguments;
 
+	@Autowired
+	private MovableProcessor movableProcessor;
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.boot.CommandLineRunner#run(java.lang.String[])
+	 */
 	@Override
 	public void run(String... args) throws Exception {
-		commandFileReader.read(commandFile);
+		if (applicationArguments.containsOption("file")) {
+			List<String> files = applicationArguments.getNonOptionArgs();
+			for (String file : files) {
+				movableProcessor.process(file);
+			}
+		} else {
+			log.info("Nothing to do !");
+		}
 	}
 
 }
